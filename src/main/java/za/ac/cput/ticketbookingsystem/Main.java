838ab5a877c4aa013c3d797728f216e60df1416d
Mustapha Saidi <msaidi@uwc.ac.za>
2021-04-08 16:00:18 +0200

    First commit

---
+++ src/main/java/za/ac/cput/ticketbookingsystem/Main.java
@@ -0,0 +1,29 @@
+/**
+ * HEADER
+ * Ticket Booking System
+ * Course: Project 2 - PRT262S
+ * Authors: Group 36 ->
+ *          CAMERON ROSE_220263159
+ *          CURSTIN JADE ROSE_220275408
+ *          MVELISO ROTO_214211983
+ *          MARTINEZ SAFARI_219325332
+ *          MOGAMMAD YAASEEN SAFODIEN_218336950
+ *          NUSSAYR EL-BAGHDADI SAIDI_220425477
+ * 
+ * Initialized
+ * JDK: 11
+ * JUnit: 5.7.0
+ */
+package za.ac.cput.ticketbookingsystem;
+
+/**
+ *
+ * @author Nussayr Saidi 220225477
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ src/test/java/za/ac/cput/ticketbookingsystem/MainTest.java
@@ -0,0 +1,63 @@
+/**
+ * HEADER
+ * Ticket Booking System
+ * Course: Project 2 - PRT262S
+ * Authors: Group 36 ->
+ *          CAMERON ROSE_220263159
+ *          CURSTIN JADE ROSE_220275408
+ *          MVELISO ROTO_214211983
+ *          MARTINEZ SAFARI_219325332
+ *          MOGAMMAD YAASEEN SAFODIEN_218336950
+ *          NUSSAYR EL-BAGHDADI SAIDI_220425477
+ * 
+ * Initialized
+ * JDK: 11
+ * JUnit: 5.7.0
+ */
+package za.ac.cput.ticketbookingsystem;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author msaidi
+ */
+public class MainTest {
+    
+    public MainTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    /**
+     * Test of main method, of class Main.
+     */
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        Main.main(args);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
