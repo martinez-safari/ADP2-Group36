d7c02f007091807f0e870c20a51b4d30a391a401
martinez safari <219325332@mycput.ac.za>
2021-04-09 00:25:10 +0200

    my changes

--- src/test/java/za/ac/cput/ticketbookingsystem/MainTest.java
+++ src/test/java/za/ac/cput/ticketbookingsystem/MainTest.java
@@ -16,10 +16,7 @@
  */
 package za.ac.cput.ticketbookingsystem;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -28,36 +25,32 @@ import static org.junit.jupiter.api.Assertions.*;
  * @author msaidi
  */
 public class MainTest {
+                
     
     public MainTest() {
-    }
-    
-    @BeforeAll
-    public static void setUpClass() {
-    }
-    
-    @AfterAll
-    public static void tearDownClass() {
-    }
-    
-    @BeforeEach
-    public void setUp() {
-    }
-    
-    @AfterEach
-    public void tearDown() {
-    }
 
-    /**
-     * Test of main method, of class Main.
-     */
-    @Test
-    public void testMain() {
-        System.out.println("main");
-        String[] args = null;
-        Main.main(args);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
+    
+     @Before
+    public void setUp() throws Exception {
+        String userName;
+        String surname;
+        String userID;
+        String dateOfBirth;
+        String countryOfBirth;
+        String cellNumber; 
+    }
+    @Test
+    public void testUser() {
+        System.out.println("main");
+        //Main.main(args);
+        // TODO review the generated test code and remove the default call to fail.
+        
+        fail("The test case is a prototype.");
+        User us=new User("name","surname","id","date of birth","country of birth","cell no");
+        System.out.println(us.toString());
+       
+    }
+   
     
 }
