41deaf6cb72f6f8199a7d9ba359c0c0005cb7a7e
martinez safari <219325332@mycput.ac.za>
2021-08-14 22:30:16 +0200

    edit the import

--- src/main/java/za/ac/cput/ticketbookingsystem/AddBooking.java
+++ src/main/java/za/ac/cput/ticketbookingsystem/AddBooking.java
@@ -4,10 +4,7 @@
  */
 package za.ac.cput.ticketbookingsystem;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
